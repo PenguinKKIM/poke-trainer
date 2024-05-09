4ffc2c446ccdcbbb8557de8d649d0a1d3893e68b
PenguinKKIM <103123935+PenguinKKIM@users.noreply.github.com>
2024-05-10 02:14:12 +0900

    feat : 라우터 개설

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,10 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
-import LoginInput from "../components/FormInput/LoginInput";
+import Home from "../pages/Home";
+import Layout from "../pages/layout";
+import LoginPage from "../pages/Login";
+import SignUpPage from "../pages/SignUp";
 
 const Router = createBrowserRouter([
   {
     path: "/",
-    element: <LoginInput />,
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "login",
+        element: <LoginPage />,
+      },
+      {
+        path: "signup",
+        element: <SignUpPage />,
+      },
+    ],
   },
 ]);
 
