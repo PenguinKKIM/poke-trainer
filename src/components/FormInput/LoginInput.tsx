d73f04093778a6a866f72134bf406153223a5828
PenguinKKIM <103123935+PenguinKKIM@users.noreply.github.com>
2024-05-13 23:24:11 +0900

    feat : 이메일 로그인 기능 추가

--- src/components/FormInput/LoginInput.tsx
+++ src/components/FormInput/LoginInput.tsx
@@ -2,51 +2,87 @@ import { NormalButton } from "../Buttons/Buttons";
 import { useState } from "react";
 import { FormContainer, Input, Label } from "./style";
 import onChangeInput from "./OnChangeInput";
+import Swal from "sweetalert2";
+import { auth } from "../../firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { FirebaseError } from "firebase/app";
+import { useNavigate } from "react-router-dom";
 
 function LoginInput() {
-  const [userId, setUserId] = useState("");
+  const navigate = useNavigate();
+
+  const [userEmail, setUserEmail] = useState("");
   const [userPassword, setUserPassword] = useState("");
 
+  const [emailError, setEmailError] = useState("");
+  const [fireBaseError, setFireBaseError] = useState("");
+
+  const [loading, setLoading] = useState(false);
+
+
   const onChangeId = (event: React.FormEvent<HTMLInputElement>) => {
-    onChangeInput(event, setUserId);
+    onChangeInput(event, setUserEmail);
   };
 
   const onChangePassword = (event: React.FormEvent<HTMLInputElement>) => {
     onChangeInput(event, setUserPassword);
   };
 
-  const onSubmitLogin = (event: React.FormEvent<HTMLFormElement>) => {
+  const onSubmitLogin = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    switch (true) {
+      case loading:
+        Swal.fire("로딩 중입니다. 잠시만 기다려 주세요.");
+        return;
+      case userEmail === "":
+        setEmailError("이메일을 입력해 주세요");
+        break;
+      default:
+        setLoading(true);
+        try {
+          await signInWithEmailAndPassword(auth, userEmail, userPassword);
+          navigate("/");
+        } catch (error) {
+          if (error instanceof FirebaseError) {
+            setFireBaseError(error.message);
+          }
+        } finally {
+          setLoading(false);
+        }
+    }
   };
 
   return (
     <FormContainer onSubmit={onSubmitLogin}>
-      <Label htmlFor="userId">
-        아이디
+      <Label htmlFor="userEmail">
+        이메일
         <Input
-          type="text"
-          name="userId"
-          value={userId}
-          id="userId"
-          placeholder="아이디를 입력하세요"
+          type="email"
+          name="userEmail"
+          value={userEmail}
+          id="userEmail"
+          placeholder="이메일을 입력하세요"
           onChange={onChangeId}
         />
+        <span>{emailError}</span>
       </Label>
       <Label htmlFor="userPassword">
         패스워드
         <Input
           type="password"
-          name="userId"
+          name="userEmail"
           value={userPassword}
           id="userPassword"
           placeholder="패스워드를 입력하세요"
           onChange={onChangePassword}
+          autoComplete="current-password"
         />
       </Label>
 
       <NormalButton type="submit" fontcolor="white" btncolor="#0000FF">
         로그인
       </NormalButton>
+      <span>{fireBaseError}</span>
     </FormContainer>
   );
 }
