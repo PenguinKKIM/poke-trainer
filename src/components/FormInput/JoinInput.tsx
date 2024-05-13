92008dd4fce9bd82c25b7144d2169ba0a076cb78
PenguinKKIM <103123935+PenguinKKIM@users.noreply.github.com>
2024-05-13 18:57:31 +0900

    feat : 이메일 회원가입 기능 구현

--- src/components/FormInput/JoinInput.tsx
+++ src/components/FormInput/JoinInput.tsx
@@ -3,14 +3,25 @@ import { NormalButton } from "../Buttons/Buttons";
 import onChangeInput from "./OnChangeInput";
 import { FormContainer, Input, Label } from "./style";
 import Swal from "sweetalert2";
+import Loading from "../Loading/Loading";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth } from "../../firebase";
+import { useNavigate } from "react-router-dom";
+import { FirebaseError } from "firebase/app";
 
 function JoinInput() {
+  const navigate = useNavigate();
   const [userId, setUserId] = useState("");
   const [joinError, setJoinError] = useState("");
+  const [emailError, setEmailError] = useState("");
   const [userPassword, setUserPassword] = useState("");
   const [userPasswordCheck, setUserPasswordCheck] = useState("");
   const [passwordError, setPasswordError] = useState("");
   const [userEmail, setUserEmail] = useState("");
+  const [passwordCheckClicked, setPasswordCheckClicked] = useState(false);
+  const [fireBaseError, setFireBaseError] = useState("");
+  const [loading, setLoading] = useState(false);
+
   const onChangeId = (event: React.FormEvent<HTMLInputElement>) => {
     onChangeInput(event, setUserId);
   };
@@ -29,11 +40,15 @@ function JoinInput() {
       case userPassword === "":
         setPasswordError("비밀번호를 입력해주세요.");
         break;
+      case userPassword.length < 6:
+        setPasswordError("비밀번호는 6자리 이상을 입력해주세요");
+        break;
       case userPassword !== userPasswordCheck:
         setPasswordError("비밀번호 확인이 일치하지 않습니다. 다시 입력해주세요.");
         break;
       default:
         setPasswordError("비밀번호 확인이 일치합니다.");
+        setPasswordCheckClicked(true);
         break;
     }
   };
@@ -42,75 +57,96 @@ function JoinInput() {
     onChangeInput(event, setUserEmail);
   };
 
-  const onSubmitJoin = (event: React.FormEvent<HTMLFormElement>) => {
+  const onSubmitJoin = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     switch (true) {
+      case loading:
+        Swal.fire("로딩 중입니다. 잠시만 기다려 주세요.");
+        return;
       case userId === "":
-        setJoinError("아이디를 입력해주세요");
+        setJoinError("닉네임을 입력해주세요");
+        break;
+      case passwordCheckClicked !== true:
+        setPasswordError("비밀번호를 확인해주세요");
         break;
       case userEmail === "":
-        setJoinError("이메일을 입력해주세요");
+        setEmailError("이메일을 입력해 주세요");
         break;
       default:
-        Swal.fire({
-          html: "<p>회원가입이 되었습니다</p>",
-        });
+        setLoading(true);
+        try {
+          const credentials = await createUserWithEmailAndPassword(auth, userEmail, userPassword);
+          console.log(credentials.user);
+          await updateProfile(credentials.user, { displayName: userId });
+          navigate("/");
+        } catch (error) {
+          if (error instanceof FirebaseError) {
+            setFireBaseError(error.message);
+          }
+        } finally {
+          setLoading(false);
+        }
     }
   };
 
   return (
     <>
-      <FormContainer onSubmit={onSubmitJoin}>
-        <Label htmlFor="userId">
-          아이디
-          <Input
-            type="text"
-            name="userId"
-            value={userId}
-            onChange={onChangeId}
-            placeholder="사용하실 아이디를 입력하세요"
-          />
-          <span>{joinError}</span>
-        </Label>
-        <Label htmlFor="password">
-          비밀번호
-          <Input
-            type="password"
-            name="password"
-            value={userPassword}
-            onChange={onChangePassword}
-            placeholder="패스워드를 입력하세요"
-          />
-        </Label>
-        <Label htmlFor="passwordCheck">
-          비밀번호 확인
-          <Input
-            type="password"
-            name="passwordCheck"
-            value={userPasswordCheck}
-            onChange={onChangePasswordCheck}
-            placeholder="패스워드를 다시 입력해주세요."
-          />
-          <span>{passwordError}</span>
-        </Label>
+      {loading ? (
+        <Loading />
+      ) : (
+        <FormContainer onSubmit={onSubmitJoin}>
+          <Label htmlFor="userId">
+            아이디
+            <Input
+              type="text"
+              name="userId"
+              value={userId}
+              onChange={onChangeId}
+              placeholder="사용하실 닉네임을 입력하세요"
+            />
+            <span>{joinError}</span>
+          </Label>
+          <Label htmlFor="password">
+            비밀번호
+            <Input
+              type="password"
+              name="password"
+              value={userPassword}
+              onChange={onChangePassword}
+              placeholder="패스워드를 입력하세요"
+            />
+          </Label>
+          <Label htmlFor="passwordCheck">
+            비밀번호 확인
+            <Input
+              type="password"
+              name="passwordCheck"
+              value={userPasswordCheck}
+              onChange={onChangePasswordCheck}
+              placeholder="패스워드를 다시 입력해주세요."
+            />
+            <span>{passwordError}</span>
+          </Label>
 
-        <NormalButton type="button" onClick={handlePasswordCheck} btncolor="#40b088">
-          비밀번호 확인
-        </NormalButton>
+          <NormalButton type="button" onClick={handlePasswordCheck} btncolor="#40b088">
+            비밀번호 확인
+          </NormalButton>
 
-        <Label htmlFor="email">
-          이메일
-          <Input
-            type="email"
-            name="email"
-            value={userEmail}
-            onChange={onChangeEmail}
-            placeholder="이메일을 입력해주세요"
-          />
-          <span>{joinError}</span>
-        </Label>
-        <NormalButton type="submit">가입하기</NormalButton>
-      </FormContainer>
+          <Label htmlFor="email">
+            이메일
+            <Input
+              type="email"
+              name="email"
+              value={userEmail}
+              onChange={onChangeEmail}
+              placeholder="이메일을 입력해주세요"
+            />
+            <span>{emailError}</span>
+          </Label>
+          <NormalButton type="submit">가입하기</NormalButton>
+          <span>{fireBaseError}</span>
+        </FormContainer>
+      )}
     </>
   );
 }
---
+++ src/firebase.tsx
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyApcA4QuE7f4QOe0n9JrCww1Dhj1NduSDI",
+  authDomain: "poke-trainer-c2836.firebaseapp.com",
+  projectId: "poke-trainer-c2836",
+  storageBucket: "poke-trainer-c2836.appspot.com",
+  messagingSenderId: "811901346338",
+  appId: "1:811901346338:web:430828bbff33f0a056e07b",
+  measurementId: "G-ZBBNZV1RBZ",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
+export const dataBase = getFirestore(app);
