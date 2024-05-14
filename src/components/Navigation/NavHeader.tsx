4beffa0e548f34ccb32646a31a8819dcc7d0f639
PenguinKKIM <103123935+PenguinKKIM@users.noreply.github.com>
2024-05-14 10:17:09 +0900

    fix : 네비게이션에 사용자에따라 상태표시 변하지 않던것 수정

--- src/components/Navigation/NavHeader.tsx
+++ src/components/Navigation/NavHeader.tsx
@@ -1,34 +1,65 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import NavBar from "./NavBar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { auth } from "../../firebase";
 import { LogoImg, NavHeaderButton, NavHeaderUl } from "./style";
-
-const currentUser = auth.currentUser;
-
-const handleLogout = () => {
-  auth.signOut();
-};
+import Swal from "sweetalert2";
 
 function NavHeader() {
+  const navigate = useNavigate();
+
   const [showNav, setShowNav] = useState(false);
+  const [currentUser, setCurrentUser] = useState(auth.currentUser);
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+    });
+    return () => unsubscribe();
+  }, []);
   const toggleNav = () => {
     setShowNav(!showNav);
   };
+
+  const handleLogout = (event: React.MouseEvent) => {
+    event.preventDefault();
+    Swal.fire({
+      html: "<p> 로그아웃 하시겠습니까? </p>",
+      showConfirmButton: true,
+      showCancelButton: true,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        auth.signOut();
+        navigate("/");
+      }
+    });
+  };
+  const handleToLogin = (event: React.MouseEvent) => {
+    event.preventDefault();
+    navigate("/login");
+  };
+
   return (
     <>
       <NavHeaderUl>
         <li>
-          <Link to="/">
+          <Link to="/" style={{ display: "flex", alignItems: "center" }}>
             <LogoImg src="image/logo/title_logo.gif" alt="피카츄" />
             포켓몬 고 트레이너
           </Link>
         </li>
         <li>
-          {
-            currentUser === null ? <NavHeaderButton><Link to="/login">로그인</Link></NavHeaderButton> :
-              <NavHeaderButton onClick={handleLogout}>로그아웃</NavHeaderButton>
-          }
+          {currentUser === null ? (
+            <NavHeaderButton onClick={handleToLogin}>
+              <span>로그인</span>
+            </NavHeaderButton>
+          ) : (
+            <>
+              <div>반갑습니다! {currentUser.displayName} 님!</div>
+              <NavHeaderButton onClick={handleLogout}>
+                <span>로그아웃</span>
+              </NavHeaderButton>
+            </>
+          )}
           <NavHeaderButton onClick={toggleNav}>
             <img src="/image/icon/ball.png" alt="네비 버튼 이미지" />
           </NavHeaderButton>
